44f728f9a711fbd7b7f3a72e843be2d685ca961f
Victor Colmenares <colmenaresvictor2797@gmail.com>
2022-03-30 20:35:25 -0400

    secc 10: ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  name: string = 'Victor';
+  value: number = 1000;
+  obj = {
+    name: 'Fernando',
+  };
+  showName() {
+    console.log(this.name);
+    console.log(this.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ButtonModule, CardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
